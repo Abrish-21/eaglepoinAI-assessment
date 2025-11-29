8cb5bee4ab4648d88ac3d33113506ec6fea139b7
Abrish-21 <abrham.tilksew@aastustudent.edu.et>
2025-11-29 10:51:33 +0300

    create a blueprint for the text analyzer class

---
+++ task_1/text_analyzer.py
@@ -0,0 +1,13 @@
+# This is the solution for task 1
+class TextAnalyser:
+    def __init__(self, text):
+
+    def get_total_word_count(self):
+        
+    def get_average_word_length(self):
+        
+    def get_longest_word(self):
+
+    def get_word_frequency(self):
+        
+        
